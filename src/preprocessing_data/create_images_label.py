0f181154031be04118b80b173380ad73d48bf1bd
Dayvison Gomes <60587044+DayvisonGomes@users.noreply.github.com>
2023-12-17 20:31:29 -0300

    Corrigindo categorização das imagens

--- src/preprocessing_data/create_images_label.py
+++ src/preprocessing_data/create_images_label.py
@@ -6,45 +6,36 @@ import cv2
 import matplotlib.pyplot as plt
 import argparse
 
-class ClassificarHistogramad(transforms.Transform):
-    def __init__(self, limiar_histograma=0.5):
-        self.limiar_histograma = limiar_histograma
+class ClassificarBrilho(transforms.Transform):
+    def __init__(self, limiar_brilho=0.3):
+        self.limiar_brilho = limiar_brilho
 
     def __call__(self, img):
-        histograma = cv2.calcHist([img], [0], None, [256], [0, 1])  # Intervalo agora é [0, 1]
-        histograma_normalizado = histograma.flatten() / np.sum(histograma)
-
-        classificacao = "Clara" if np.sum(histograma_normalizado[:128]) > self.limiar_histograma else "Escura"
-
+        brilho_medio = np.mean(img)
+        classificacao = "Clara" if brilho_medio > self.limiar_brilho else "Escura"
         return classificacao
 
-def classificar_imagem_com_transformacoes(caminho_imagem, img_transforms, limiar_histograma=0.5):
-    img = cv2.imread(caminho_imagem, cv2.IMREAD_GRAYSCALE)
-
-    # Aplicar transformações
-    img_transformed = img_transforms({"image": img})["image"]
-
-    # Classificar com base no histograma
-    img_transform = ClassificarHistogramad(limiar_histograma=limiar_histograma)
+def classificar_imagem_com_transformacoes(caminho_imagem, img_transforms, limiar_brilho=0.3):
+    img_transformed = img_transforms({"image": caminho_imagem})["image"]
+    img_transform = ClassificarBrilho(limiar_brilho=limiar_brilho)
     classificacao = img_transform(img_transformed)
 
     return classificacao
 
-def percorrer_tsv_e_classificar(tsv_path, img_transforms, limiar_histograma=0.5):
+def percorrer_tsv_e_classificar(tsv_path, img_transforms, limiar_brilho=0.3):
     df = pd.read_csv(tsv_path, sep="\t")
 
     classificacoes = []
 
     for index, row in df.iterrows():
         caminho_imagem = str(row["image"])
-        classificacao = classificar_imagem_com_transformacoes(caminho_imagem, img_transforms, limiar_histograma=limiar_histograma)
+        classificacao = classificar_imagem_com_transformacoes(caminho_imagem, img_transforms, limiar_brilho=limiar_brilho)
         classificacoes.append({"image": caminho_imagem, "classificacao": classificacao})
 
     return classificacoes
 
 def chage_tables_ids(args):
     input_dir = args.input_dir
-    
     train_data_tsv_path = os.path.join(input_dir, "train.tsv")
     val_data_tsv_path = os.path.join(input_dir, "validation.tsv")
     test_data_tsv_path = os.path.join(input_dir, "test.tsv")
@@ -60,11 +51,10 @@ def chage_tables_ids(args):
     classificacoes_val = percorrer_tsv_e_classificar(val_data_tsv_path, img_transforms)
     classificacoes_test = percorrer_tsv_e_classificar(test_data_tsv_path, img_transforms)
 
-    # Salvar os resultados
     pd.DataFrame(classificacoes_train).to_csv(os.path.join(input_dir, "classificacoes_train.csv"), index=False)
     pd.DataFrame(classificacoes_val).to_csv(os.path.join(input_dir, "classificacoes_val.csv"), index=False)
     pd.DataFrame(classificacoes_test).to_csv(os.path.join(input_dir, "classificacoes_test.csv"), index=False)
-
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", help="Caminho em que os arquivos .tsv estão")
